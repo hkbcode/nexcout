9a753769793abb04b8ee1f51a3475e5bb20af04a
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2021-04-26 10:55:27 +0530

    initial commit :fire:

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-gray-400 uppercase"> Start Building apps with Nextjs and Tailwindcss </p>
+    </div>
+  )
+}
